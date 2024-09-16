29ca4ad3781e0d1894cdcefdbc16fffb19a3ea6c
anildixit01 <anildixit01@gmail.com>
2024-09-16 08:11:36 +0530

    task 01 and task 2 13092024 have done completed.

---
+++ src/sept/ex_13092024/task02.java
@@ -0,0 +1,32 @@
+package sept.ex_13092024;
+
+public class task02 {
+    public static void main(String[] args){
+
+   double a = 34;
+   double b = 10;
+
+        System.out.printf("%f + %f = %f\n",a,b,a+b);
+        System.out.printf("%f - %f = %f\n",a,b,a-b);
+        System.out.printf("%f * %f = %f\n",a,b,a*b);
+        System.out.printf("%f / %f = %f\n",a,b,a/b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
