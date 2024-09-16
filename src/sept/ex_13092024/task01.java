31f9d20d146692b1592307a99865a8e4a422f82c
anildixit01 <anildixit01@gmail.com>
2024-09-16 09:00:03 +0530

    task 01 and task 2 13092024 have done completed.

--- src/sept/ex_13092024/task01.java
+++ src/sept/ex_13092024/task01.java
@@ -3,7 +3,7 @@ package sept.ex_13092024;
 public class task01 {
     public static void main(String [] args){
         int num = 10;
-        System.out.printf("%d * %d = %d",num,1,num*1);
+        System.out.printf("%d * %d = %d",num,1,num);
         System.out.println();
         System.out.printf("%d * %d = %d",num,2,num*2);
         System.out.println();
