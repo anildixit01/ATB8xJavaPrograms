ff2ef7d4c1810cb556d9d9b218fe7c5c03a1e418
anildixit01 <anildixit01@gmail.com>
2024-09-27 22:17:24 +0530

    Task_20092024_completed

---
+++ src/Task_20092024/Task5_Leap_year.java
@@ -0,0 +1,13 @@
+package Task_20092024;
+
+public class Task5_Leap_year {
+    public static void main(String[] args) {
+        int year= 2024; // we can change  year
+        if(year%4==0 && year%100!=0 || year%400==0){
+            System.out.println("Leap year");
+        }else{
+            System.out.println("Not a leap year");
+        }
+
+    }
+}
