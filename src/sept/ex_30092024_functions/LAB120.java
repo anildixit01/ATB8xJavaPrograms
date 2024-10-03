035023a9e565166dc2fb24ee444c7ea39ac9f109
anildixit01 <anildixit01@gmail.com>
2024-10-03 19:46:09 +0530

    30092024 all file complited

---
+++ src/sept/ex_30092024_functions/LAB120.java
@@ -0,0 +1,18 @@
+package sept.ex_30092024_functions;
+
+public class LAB120 {
+    public static void main(String[] args) {
+        // // Functions
+        // Block of code which we can reuse.
+        // 1. Pre-built - Math.max() - Java guys
+        // 2. User created / defined
+
+
+        // Two component of the functions
+        // 1. Define
+        // 2. Call the function
+
+
+    }
+
+}
---
+++ src/sept/ex_30092024_functions/Lab123.java
@@ -0,0 +1,8 @@
+package sept.ex_30092024_functions;
+
+public class Lab123 {
+    public static void main(String[] args) {
+
+
+    }
+}
