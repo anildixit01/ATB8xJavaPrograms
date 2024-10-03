a4adc38cdb674262720fa81d30d771b3c358560b
anildixit01 <anildixit01@gmail.com>
2024-10-03 19:45:10 +0530

    27092024 all file complited

--- src/sept/ex_27092024/Task3.java
+++ src/sept/ex_27092024/Task3.java
@@ -4,13 +4,20 @@ public class Task3 {
     public static void main(String[] args) {
 
         //Write a Java program that calculates the sum of numbers from 1 to 100 using a while loop.
-        int a=1;
+      int a=1;
         int sum= 0;
-        while (a<=100){
-            sum= sum+a;
+       /* while (a<=100){
+          sum= sum+a;
             a++;
         }
+      System.out.println("the sum of numbers from 1 to 100 is :" + sum);*/
+
+       do{
+            sum  = sum+ a;
+            a++;
+        }while (a<=100);
         System.out.println("the sum of numbers from 1 to 100 is :" + sum);
+
 
 
     }
