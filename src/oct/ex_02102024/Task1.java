6a517c091a9ca6f25c29fec8d6280250f99b06d6
anildixit01 <anildixit01@gmail.com>
2024-10-04 07:00:50 +0530

    02102024 all file complited

---
+++ src/oct/ex_02102024/Task1.java
@@ -0,0 +1,23 @@
+package oct.ex_02102024;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+      /*  1.Task related to Class and Objects:
+
+        Classes are blueprint/templates.
+
+                Object is an instance of a class.
+
+        Multiple objects can be defined for a class.
+
+        Class has attributes and methods.
+
+                Reference until not declared ,memory of Object is initialized to null.
+
+                New keyword is used to define a class.
+
+        
+    }
+}
---
+++ src/oct/ex_02102024/Task3.java
@@ -0,0 +1,25 @@
+package oct.ex_02102024;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            //3.Create a Class  ATBStudent and 5 Objects - (A/B) - github.com
+
+            ATBStudent object1=new ATBStudent();
+            object1.age=30;
+
+            ATBStudent object2=new ATBStudent();
+            object2.name="Anil";
+
+            ATBStudent object3=new ATBStudent();
+            object3.phone=8077109101L;
+
+            ATBStudent object4=new ATBStudent();
+            object4.batch=9;
+
+            ATBStudent object5=new ATBStudent();
+            object5.address="Firozabad";
+
+        }
+}
