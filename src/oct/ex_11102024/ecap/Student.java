90b59b699b65f7da16c7a8aa8e2964a495c1e059
anildixit01 <anildixit01@gmail.com>
2024-10-15 20:32:08 +0530

    01102024 all files complited

---
+++ src/oct/ex_11102024/ecap/Student.java
@@ -0,0 +1,45 @@
+package oct.ex_11102024.ecap;
+
+public class Student {
+    // instance varibles are private
+    private String Name;
+    private int age;
+    public String address;
+
+    public Student(String name, int age, String address) {
+        Name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+//Public getter and setter ,method
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address, boolean isAdmin) {
+        if (isAdmin) {
+            this.address = address;
+        } else {
+            System.out.println("Not Allowed");
+        }
+
+    }
+}
