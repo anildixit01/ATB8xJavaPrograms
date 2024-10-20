486c878e1f8fe0a726745d438fbeb46ac603c699
anildixit01 <anildixit01@gmail.com>
2024-10-20 09:43:27 +0530

    01602024 all files complited

---
+++ src/oct/ex_18102024_Strings/Lab159.java
@@ -0,0 +1,16 @@
+package oct.ex_18102024_Strings;
+
+public class Lab159 {
+    public static void main(String[] args) {
+
+       // char a= 'A';
+        String name = "Pramod"; // Contains multiple chars.
+        name = "Amit";
+        name = "Dutta"; // SCP
+
+        String s1 = new String("Pramod"); // Object Area or heap area
+        String s2 = new String("Pramod"); // // Object Area
+
+
+    }
+}
---
+++ src/oct/ex_18102024_Strings/Lab164.java
@@ -0,0 +1,11 @@
+package oct.ex_18102024_Strings;
+
+public class Lab164 {
+    public static void main(String[] args) {
+        String str1 = "Hello"; // best example of immutability
+        str1.concat(" World"); // new string created
+        System.out.println(str1); // out put Hello
+
+
+    }
+}
