9c7fbb6cd579a5588e32212723d0d27cf846bf8e
anildixit01 <anildixit01@gmail.com>
2024-10-27 21:22:46 +0530

    25102024_All Files are commited

--- src/oct/ex_18102024_Strings/Lab163_StringBuilder_StringBuffer.java
+++ src/oct/ex_18102024_Strings/Lab163_StringBuilder_StringBuffer.java
@@ -7,13 +7,13 @@ public class Lab163_StringBuilder_StringBuffer {
 //        name = "Aramod";
 
         // final class StringBuffer
-        StringBuffer stringBuffer = new StringBuffer("Pramod");
+     /*   StringBuffer stringBuffer = new StringBuffer("Pramod");
         StringBuffer stringBuffer1 = new StringBuffer("Dutta");
         stringBuffer.append("Dutta");
         stringBuffer1.append("Pramod");
         System.out.println(stringBuffer);
         System.out.println(stringBuffer1);
-
+*/
 
         // C1, C2 -> Toy
         //  C1 - Toy 2 -> C2 Toy ->2 - Syn - Thread Safty - Slow
